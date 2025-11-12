9f1096cc76c30bae67aa502647f933f72282f0e4
Peter Cornelis <poltertec@gmail.com>
2025-11-12 11:09:04 +0100

    minor visual tweak

--- resources/views/components/equity-card-dashboard.blade.php
+++ resources/views/components/equity-card-dashboard.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="grid grid-flow-row text-right">
             <span>1510 USD</span>
-            <p class="text-xs">+ 15 USD <span class="bg-notice text-white rounded py-px px-1 shadow">1%</span></p>
+            <p class="text-xs">+ 15 USD <span class="bg-notice text-white font-semibold rounded py-px px-1 shadow">1 %</span></p>
         </div>
     </a>
 </li>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
         <article class="flex justify-between max-w-3xl mx-auto mt-10 px-6 py-4 bg-white rounded-lg shadow">
             <ul>
                 <li title="Totaal fictief vermogen"><h3 class="text-2xl font-bold">10.010 USD</h3></li>
-                <li title="Fictief gerealiseerde winst/verlies" class="text-sm">+ 10 USD <span class="bg-notice text-white rounded py-px px-1 shadow">0.1%</span> Sinds start</li>
+                <li title="Fictief gerealiseerde winst/verlies" class="text-sm">+ 10 USD <span class="bg-notice text-white font-semibold rounded py-px px-1 shadow">0.1 %</span> Sinds start</li>
                 <li title="Fictieve cash positie" class="text-sm pt-2">Cash: 2400 USD</li>
             </ul>
             <a class="flex flex-col items-center">
