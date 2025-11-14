b9fceff4056e9ce61243f090d7fec6a28d4af308
Peter Cornelis <poltertec@gmail.com>
2025-11-14 14:32:56 +0100

    init EquityService

---
+++ app/Services/EquityService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class EquityService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(private FmpService $fmpService)
+    {
+    }
+}
