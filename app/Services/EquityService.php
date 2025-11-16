759e57904c130c3ef2063fc28e7f0edc042b320a
Peter Cornelis <poltertec@gmail.com>
2025-11-16 22:37:07 +0100

    implement ObjectBuilder

--- app/Services/EquityService.php
+++ app/Services/EquityService.php
@@ -2,11 +2,7 @@
 
 namespace App\Services;
 
-use App\Models\Chart;
-use App\Models\Company;
 use App\Models\Equity;
-use App\Models\Exchange;
-use App\Models\FinancialRatio;
 use Illuminate\Validation\ValidationException;
 
 class EquityService
@@ -14,7 +10,7 @@ class EquityService
     /**
      * Create a new class instance.
      */
-    public function __construct(private FmpService $fmpService)
+    public function __construct(private FmpService $fmpService, private ObjectBuilder $objectBuilder)
     {
     }
 
@@ -30,52 +26,10 @@ class EquityService
         $FinancialRatios = $this->fmpService->getFinancialRatios($symbol);
         $historicalPrices = $this->fmpService->getHistoricalPrices($symbol);
 
-        $exchange = Exchange::updateOrCreate(
-            ['code' => $profile['exchange']],
-            [
-                'name' => $profile['exchangeFullName'],
-                'currency' => $profile['currency']
-            ]
-        );
-
-        $company = Company::create([
-            'name' => $profile['companyName'],
-            'website' => $profile['website'],
-            'sector' => $profile['sector'] ,
-            'industry' => $profile['industry'],
-            'country' => $profile['country'],
-            'image' => $profile['image'],
-        ]);
-
-        $company->exchanges()->syncWithoutDetaching($exchange->id);
-
-        $equity = Equity::create([
-            'isin' => $profile['isin'],
-            'symbol' => $symbol,
-            'company_id' => $company->id,
-            'exchange_id' => $exchange->id,
-            'lastDividend' => $profile['lastDiv'] ?? 0
-        ]);
-
-        FinancialRatio::create([
-            'equity_id' => $equity->id,
-            'date' => $FinancialRatios['date'],
-            'beta' => $profile['beta'],
-            'priceToEarningsRatio' => $FinancialRatios['priceToEarningsRatio'],
-            'priceToBookRatio' => $FinancialRatios['priceToBookRatio'],
-            'dividendYieldPercentage' => $FinancialRatios['dividendYieldPercentage'],
-            'currentRatio' => $FinancialRatios['currentRatio'],
-            'revenuePerShare' => $FinancialRatios['revenuePerShare'],
-        ]);
-
-        $equity->charts()->createMany(
-            collect($historicalPrices)
-                ->map(fn($item) => [
-                    'date' => $item['date'],
-                    'price' => $item['price'],
-                    'volume' => $item['volume'],
-                ])
-                ->toArray()
-        );
+        $exchange = $this->objectBuilder->exchange($profile);
+        $company = $this->objectBuilder->company($profile, $exchange->id);
+        $equity = $this->objectBuilder->equity($profile, $company->id, $exchange->id);
+        $this->objectBuilder->financialRatio($equity->id,$profile['beta'], $FinancialRatios['date']);
+        $this->objectBuilder->charts($equity, $historicalPrices);
     }
 }
---
+++ app/Services/ObjectBuilder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Company;
+use App\Models\Equity;
+use App\Models\Exchange;
+use App\Models\FinancialRatio;
+
+class ObjectBuilder
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+    }
+
+    public function exchange(array $exchangeData): Exchange
+    {
+        return Exchange::firstOrCreate(
+            ['code' => $exchangeData['code']],
+            [
+                'name' => $exchangeData['name'],
+                'currency' => $exchangeData['currency']
+            ]
+        );
+    }
+
+    public function company(array $companyData, int $exchangeId): Company
+    {
+        $company = Company::updateOrCreate([
+            'name' => $companyData['name'],
+            'website' => $companyData['website'],
+            'sector' => $companyData['sector'] ,
+            'industry' => $companyData['industry'],
+            'country' => $companyData['country'],
+            'image' => $companyData['image'],
+        ]);
+
+        if (!$company->exchanges->contains($exchangeId)) {
+            $company->exchanges()->attach($exchangeId);
+        }
+
+        return $company;
+    }
+
+    public function equity(array $equityData, int $companyId, int $exchangeId): Equity
+    {
+        return Equity::updateOrCreate([
+            'isin' => $equityData['isin'],
+            'symbol' => $equityData['symbol'],
+            'company_id' => $companyId,
+            'exchange_id' => $exchangeId,
+            'lastDividend' => $equityData['lastDividend']
+        ]);
+    }
+
+    public function financialRatio(int $equityId, string $beta, array $FinancialRatios)
+    {
+        FinancialRatio::updateOrCreate([
+            'equity_id' => $equityId,
+            'date' => $FinancialRatios['date'],
+            'beta' => $beta,
+            'priceToEarningsRatio' => $FinancialRatios['priceToEarningsRatio'],
+            'priceToBookRatio' => $FinancialRatios['priceToBookRatio'],
+            'dividendYieldPercentage' => $FinancialRatios['dividendYieldPercentage'],
+            'currentRatio' => $FinancialRatios['currentRatio'],
+            'revenuePerShare' => $FinancialRatios['revenuePerShare'],
+        ]);
+    }
+
+    public function charts(Equity $equity, array $historicalPrices)
+    {
+        $equity->charts()->createMany(
+            array_map(fn($item) => [
+                'date' => $item['date'],
+                'price' => $item['price'],
+                'volume' => $item['volume'],
+            ], $historicalPrices)
+        );
+    }
+}
